231a87ab364f17def0356845977e731843f9e770
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-01 17:38:13 -0700

    continued improving styling and responsiveness. Footer now at bottom instead of floating in center of screen.

--- react-app/src/Components/Header.js
+++ react-app/src/Components/Header.js
@@ -9,25 +9,20 @@ export default function Header() {
   return (
     <>
     <Flex alignItems="top" display="block" flexDirection="column" justifyContent="flex-end" background-color="black" color="white">
-          <h1>Infinite Jokes Generator 3.0</h1>
+          <h1>Infinite Jokes Generator</h1>
       </Flex>
       <Flex alignItems="top" flexDirection="row" justifyContent="space-around">
             <div></div>
             <div></div>
-            <div><Link to="/About"><Emoji symbol="✨" label="sparkles"/>
-ABOUT<Emoji symbol="✨" label="sparkles"/></Link></div>
+            <div className="header-link"><Link to="/About"><Emoji symbol="✨" label="sparkles"/>
+ ABOUT <Emoji symbol="✨" label="sparkles"/></Link></div>
             <br />
-            <div><Link to="/App"><Emoji symbol="✨" label="sparkles"/>JOKES<Emoji symbol="✨" label="sparkles"/></Link></div>
+            <div className="header-link"><Link to="/App"><Emoji symbol="✨" label="sparkles"/>JOKES<Emoji symbol="✨" label="sparkles"/></Link></div>
             <br />
-              <div><Link to="/Rainbow"><Emoji symbol="✨" label="sparkles"/>RAINBOW<Emoji symbol="✨" label="sparkles"/></Link></div>
+              <div className="header-link"><Link to="/Rainbow"><Emoji symbol="✨" label="sparkles"/>RAINBOW<Emoji symbol="✨" label="sparkles"/></Link></div>
               <div></div>
               <div></div>
       </Flex>
-    <Flex alignItems="top" display="block" flexDirection="column" justifyContent="flex-end">
-       <br />
-       <span><em>~ a place to be delighted ~ </em></span>
-       <br />
-    </Flex>
         </>
   );
 }
--- react-app/src/Components/RainbowDivs.js
+++ react-app/src/Components/RainbowDivs.js
@@ -13,16 +13,27 @@ export default function RainbowDivs() {
       <Flex flex-wrap="wrap" flexDirection="row" justify-content="space-around" align-items="center">
    <>
                    <div className="rainbowbox" style={{background: "#DC143C"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#FF4500"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#0000FF"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#4B0082"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#8F00FF"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#483D8B"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#20B2AA"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#1E90FF"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#BC8F8F"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#FF1493"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#3CB371"}}></div>
+                   <div> </div>
                    <div className="rainbowbox" style={{background: "#FF8C00"}}></div>
 </>
 
